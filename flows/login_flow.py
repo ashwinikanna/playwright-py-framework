d83f1f33fcd0f12b4474718c7d846577888dd891
AshwiniKanna <ashu.vkiran@gmail.com>
2026-02-19 21:10:14 -0800

    Stablize login flow by navigating directly to /auth/login

--- flows/login_flow.py
+++ flows/login_flow.py
@@ -6,8 +6,7 @@ class LoginFlow:
         self.login_page = LoginPage(page)
 
     def login(self, base_url, email, password):
-        self.login_page.goto(base_url)
-        self.login_page.open_sign_in()
+        self.login_page.goto(f"{base_url}/auth/login") 
         self.login_page.enter_email(email)
         self.login_page.enter_password(password)
         self.login_page.submit()
